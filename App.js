91d055409d3b1e5f5105895a059addb47e54bcd3
katherineli29 <katherineli29@g.ucla.edu>
2023-07-06 00:59:29 -0700

    initial commit

--- App.js
+++ App.js
@@ -4,296 +4,10 @@ import { useState, useEffect } from 'react';
 import ReactWordcloud from 'react-wordcloud';
 import "d3-transition";
 import { select } from "d3-selection";
-
-const original = [
-    {
-        text: 'Lorem',
-        value: 64,
-    },
-    {
-        text: 'ipsum',
-        value: 11,
-    },
-    {
-        text: 'dolor',
-        value: 16,
-    },
-    {
-        text: 'sit',
-        value: 17,
-    },
-    {
-        text: 'amet',
-        value: 10,
-    },
-    {
-        text: 'consectetur',
-        value: 54,
-    },
-    {
-        text: 'adipiscing',
-        value: 12,
-    },
-    {
-        text: 'elit',
-        value: 77,
-    },
-    {
-        text: 'sed',
-        value: 45,
-    },
-    {
-        text: 'do',
-        value: 19,
-    },
-    {
-        text: 'eiusmod',
-        value: 13,
-    },
-    {
-        text: 'tempor',
-        value: 32,
-    },
-    {
-        text: 'incididunt',
-        value: 22,
-    },
-    {
-        text: 'ut',
-        value: 35,
-    },
-    {
-        text: 'labore',
-        value: 24,
-    },
-    {
-        text: 'et',
-        value: 38,
-    },
-    {
-        text: 'dolore',
-        value: 70,
-    },
-    {
-        text: 'magna',
-        value: 26,
-    },
-    {
-        text: 'aliqua',
-        value: 14,
-    },
-    {
-        text: 'enim',
-        value: 29,
-    },
-    {
-        text: 'ad',
-        value: 41,
-    },
-    {
-        text: 'minim',
-        value: 49,
-    },
-    {
-        text: 'veniam',
-        value: 20,
-    },
-    {
-        text: 'quiz',
-        value: 59,
-    },
-    {
-        text: 'nostrud',
-        value: 49,
-    },
-    {
-        text: 'felt',
-        value: 45,
-    },
-    {
-        text: 'exercitation',
-        value: 11,
-    },
-    {
-        text: 'ullamco',
-        value: 22,
-    },
-    {
-        text: 'laboris',
-        value: 12,
-    },
-    {
-        text: 'free',
-        value: 38,
-    },
-    {
-        text: 'nisi',
-        value: 54,
-    },
-    {
-        text: 'alliquip',
-        value: 14,
-    },
-    {
-        text: 'ex',
-        value: 41,
-    },
-    {
-        text: 'root',
-        value: 24,
-    },
-    {
-        text: 'ea',
-        value: 16,
-    },
-    {
-        text: 'commodo',
-        value: 29,
-    },
-    {
-        text: 'Duis',
-        value: 20,
-    },
-    {
-        text: 'aute',
-        value: 10,
-    },
-    {
-        text: 'irure',
-        value: 13,
-    },
-    {
-        text: 'in',
-        value: 35,
-    },
-    {
-        text: 'reprehenderit',
-        value: 59,
-    },
-    {
-        text: 'voluptate',
-        value: 32,
-    },
-    {
-        text: 'velit',
-        value: 17,
-    },
-    {
-        text: 'dr',
-        value: 77,
-    },
-    {
-        text: 'esse',
-        value: 19,
-    },
-    {
-        text: 'cillum',
-        value: 26,
-    },
-    {
-        text: 'eu',
-        value: 17,
-    },
-    {
-        text: 'fugiat',
-        value: 22,
-    },
-    {
-        text: 'hour',
-        value: 35,
-    },
-    {
-        text: 'nulla',
-        value: 38,
-    },
-    {
-        text: 'pariatur',
-        value: 11,
-    },
-    {
-        text: 'sint',
-        value: 13,
-    },
-    {
-        text: 'Excepteur',
-        value: 10,
-    },
-    {
-        text: 'occaecat',
-        value: 49,
-    },
-    {
-        text: 'cupidatat',
-        value: 19,
-    },
-    {
-        text: 'non',
-        value: 20,
-    },
-    {
-        text: 'proident',
-        value: 64,
-    },
-    {
-        text: 'care',
-        value: 54,
-    },
-    {
-        text: 'minute',
-        value: 29,
-    },
-    {
-        text: 'culpa',
-        value: 16,
-    },
-    {
-        text: 'qui',
-        value: 59,
-    },
-    {
-        text: 'officia',
-        value: 49,
-    },
-    {
-        text: 'deserunt',
-        value: 24,
-    },
-    {
-        text: 'mollit',
-        value: 19,
-    },
-    {
-        text: 'anim',
-        value: 20,
-    },
-    {
-        text: 'id',
-        value: 29,
-    },
-    {
-        text: 'est',
-        value: 17,
-    },
-    {
-        text: 'laborum',
-        value: 26,
-    },
-    {
-        text: 'sure',
-        value: 38,
-    },
-    {
-        text: 'work',
-        value: 64,
-    },
-    {
-        text: 'pair',
-        value: 11,
-    },
-];
+import original from './original.js';
 
 let words = original;
-
+const size = [600, 400];
 function getCallback(callback) {
     return function (word, event) {
         const isActive = callback !== "onWordMouseOut";
@@ -363,17 +77,23 @@ function App() {
             <h1>Word Cloud</h1>
             <p>Generate beautiful word clouds out of any text!</p>
 
-            <div>
-                <textarea
-                    id="txtArea"
-                    placeholder='Enter the text you would like to make into a word cloud here'
-                    ></textarea>
-            </div>
+            <div class="grid-container">
 
-            <button id="enter" onClick={getText}>Enter</button>
+                <div class="grid-child text">
+                    <textarea
+                        id="txtArea"
+                        placeholder='Enter the text you would like to make into a word cloud here'
+                    ></textarea>
+
+                    <p>
+                            <button id="enter" onClick={getText}>Enter</button>
+                    </p>
+                </div>
              
-            <div>
-                <ReactWordcloud callbacks={callbacks} words={words} />
+            <div id="cloud" class="grid-child cloud">
+                    <ReactWordcloud callbacks={callbacks} size={size} words={words} />
+                </div>
+
             </div>
 
             <footer>created by Katherine Li</footer>
---
+++ original.js
@@ -0,0 +1,286 @@
+export default [
+    {
+        text: 'Lorem',
+        value: 64,
+    },
+    {
+        text: 'ipsum',
+        value: 11,
+    },
+    {
+        text: 'dolor',
+        value: 16,
+    },
+    {
+        text: 'sit',
+        value: 17,
+    },
+    {
+        text: 'amet',
+        value: 10,
+    },
+    {
+        text: 'consectetur',
+        value: 54,
+    },
+    {
+        text: 'adipiscing',
+        value: 12,
+    },
+    {
+        text: 'elit',
+        value: 77,
+    },
+    {
+        text: 'sed',
+        value: 45,
+    },
+    {
+        text: 'do',
+        value: 19,
+    },
+    {
+        text: 'eiusmod',
+        value: 13,
+    },
+    {
+        text: 'tempor',
+        value: 32,
+    },
+    {
+        text: 'incididunt',
+        value: 22,
+    },
+    {
+        text: 'ut',
+        value: 35,
+    },
+    {
+        text: 'labore',
+        value: 24,
+    },
+    {
+        text: 'et',
+        value: 38,
+    },
+    {
+        text: 'dolore',
+        value: 70,
+    },
+    {
+        text: 'magna',
+        value: 26,
+    },
+    {
+        text: 'aliqua',
+        value: 14,
+    },
+    {
+        text: 'enim',
+        value: 29,
+    },
+    {
+        text: 'ad',
+        value: 41,
+    },
+    {
+        text: 'minim',
+        value: 49,
+    },
+    {
+        text: 'veniam',
+        value: 20,
+    },
+    {
+        text: 'quiz',
+        value: 59,
+    },
+    {
+        text: 'nostrud',
+        value: 49,
+    },
+    {
+        text: 'felt',
+        value: 45,
+    },
+    {
+        text: 'exercitation',
+        value: 11,
+    },
+    {
+        text: 'ullamco',
+        value: 22,
+    },
+    {
+        text: 'laboris',
+        value: 12,
+    },
+    {
+        text: 'free',
+        value: 38,
+    },
+    {
+        text: 'nisi',
+        value: 54,
+    },
+    {
+        text: 'alliquip',
+        value: 14,
+    },
+    {
+        text: 'ex',
+        value: 41,
+    },
+    {
+        text: 'root',
+        value: 24,
+    },
+    {
+        text: 'ea',
+        value: 16,
+    },
+    {
+        text: 'commodo',
+        value: 29,
+    },
+    {
+        text: 'Duis',
+        value: 20,
+    },
+    {
+        text: 'aute',
+        value: 10,
+    },
+    {
+        text: 'irure',
+        value: 13,
+    },
+    {
+        text: 'in',
+        value: 35,
+    },
+    {
+        text: 'reprehenderit',
+        value: 59,
+    },
+    {
+        text: 'voluptate',
+        value: 32,
+    },
+    {
+        text: 'velit',
+        value: 17,
+    },
+    {
+        text: 'dr',
+        value: 77,
+    },
+    {
+        text: 'esse',
+        value: 19,
+    },
+    {
+        text: 'cillum',
+        value: 26,
+    },
+    {
+        text: 'eu',
+        value: 17,
+    },
+    {
+        text: 'fugiat',
+        value: 22,
+    },
+    {
+        text: 'hour',
+        value: 35,
+    },
+    {
+        text: 'nulla',
+        value: 38,
+    },
+    {
+        text: 'pariatur',
+        value: 11,
+    },
+    {
+        text: 'sint',
+        value: 13,
+    },
+    {
+        text: 'Excepteur',
+        value: 10,
+    },
+    {
+        text: 'occaecat',
+        value: 49,
+    },
+    {
+        text: 'cupidatat',
+        value: 19,
+    },
+    {
+        text: 'non',
+        value: 20,
+    },
+    {
+        text: 'proident',
+        value: 64,
+    },
+    {
+        text: 'care',
+        value: 54,
+    },
+    {
+        text: 'minute',
+        value: 29,
+    },
+    {
+        text: 'culpa',
+        value: 16,
+    },
+    {
+        text: 'qui',
+        value: 59,
+    },
+    {
+        text: 'officia',
+        value: 49,
+    },
+    {
+        text: 'deserunt',
+        value: 24,
+    },
+    {
+        text: 'mollit',
+        value: 19,
+    },
+    {
+        text: 'anim',
+        value: 20,
+    },
+    {
+        text: 'id',
+        value: 29,
+    },
+    {
+        text: 'est',
+        value: 17,
+    },
+    {
+        text: 'laborum',
+        value: 26,
+    },
+    {
+        text: 'sure',
+        value: 38,
+    },
+    {
+        text: 'work',
+        value: 64,
+    },
+    {
+        text: 'pair',
+        value: 11,
+    },
+];
